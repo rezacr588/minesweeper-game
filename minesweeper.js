cafed699552b389706a3acfcf593e4c2d14a7020
Reza Zeraat <rezacr588@gmail.com>
2021-08-10 01:26:52 +0430

    Populate a board with mines and tiles

--- minesweeper.js
+++ minesweeper.js
@@ -1,18 +1,58 @@
 // Logic
 
+const TILE_STATUSES = {
+  HIDDEN: 'hidden',
+  MINE: 'mine',
+  NUMBER: 'number',
+  MARKED: 'marked'
+}
+
 // Build two dimensions grid
 export function createBoard(boardSize, numberOfMines) {
-  const boards = []
-
+  const board = []
+  const minePositions = getMinePositions(boardSize, numberOfMines)
   for (let x = 0; x < boardSize; x++) {
     const row = []
     for (let y = 0; y < boardSize; y++) {
+      const element = document.createElement('div')
+      element.dataset.status = TILE_STATUSES.HIDDEN
       const tile = {
-        x , y
+        element,
+        x,
+        y,
+        mine: minePositions.some(positionMatch.bind(null, {x,y})),
+        get status() {
+          return this.element.dataset.status
+        },
+        set status(value) {
+          this.element.dataset.status = value
+        }
       }
       row.push(tile)
     }
-    boards.push(row)
+    board.push(row)
   }
-  return boards
+  return board
+}
+
+function getMinePositions(boardSize, numberOfMines) {
+  const positions = []
+  while (positions.length < numberOfMines) {
+    const position = {
+      x: randomNumber(boardSize),
+      y: randomNumber(boardSize),
+    }
+    if (!positions.some(positionMatch.bind(null, position))) {
+      positions.push(position)
+    }
+  }
+  return positions
+}
+
+function positionMatch(a, b) {
+  return a.x === b.x && a.y === b.y
+}
+
+function randomNumber(size) {
+  return Math.floor(Math.random() * size)
 }
--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-import { createBoard } from "./minesweeper"
+import { createBoard } from "./minesweeper.js"
 
-createBoard(2, 2)
+const BOARD_SIZE = 10
+const NUMBER_OF_MINES = 2
 
-// 1. Populate a board with mines and tiles
+const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES)
+
+const boardElement = document.querySelector(".board")
+
+board.forEach(row => {
+  row.forEach(tile => {
+    boardElement.append(tile.element)
+  })
+})
+
+boardElement.style.setProperty("--size",BOARD_SIZE)
+
 // 2. Left click on tiles
   // a. Reveal tiles
 // 3. Right click on tiles
